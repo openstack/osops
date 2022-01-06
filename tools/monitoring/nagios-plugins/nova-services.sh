08de36aaee61172787d7285e371cb40c0cae80dc
Cyril Lopez <cylopez@redhat.com>
2022-01-06 14:31:11 +0100

    [fix] false positive on nova-services
    Nova-services have a false positive due to index of return table contain
    the word "down" for column "Forced down"
    
    Tested on Xena
    
    Story: 2008299
    Task: 41184
    
    Change-Id: I4ffecb21079c04ca1a7976bea06544dad271d851
    Signed-off-by: Cyril Lopez <cylopez@redhat.com>

--- tools/monitoring/nagios-plugins/nova-services.sh
+++ tools/monitoring/nagios-plugins/nova-services.sh
@@ -2,7 +2,7 @@
 
 source ~/openrc
 
-output=$(nova service-list | grep down)
+output=$(nova service-list | tail -n +3 | grep down)
 if [ $? -eq 0 ]; then
     echo -n "CRITICAL - OpenStack Nova services down: "
     echo "${output}" | awk '{print $2,$4}' | while read LINE; do
